f74765354b5248d11afd7c8f65c861af6d5c1dc1
Zusefi <z.usefi2020nist@gmail.com>
2026-07-15 19:55:43 +0330

    Day 01 - HTTP fundamentals and request inspection API

---
+++ day01-http-fundamentals/HttpLearningApi/Controllers/HttpDebugController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace HttpLearningApi.Controllers;
+
+[ApiController]
+[Route("debug")]
+public class HttpDebugController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult InspectRequest()
+    {
+        var result = new
+        {
+            Method = Request.Method,
+            Path = Request.Path,
+            Query = Request.Query.ToDictionary(q => q.Key, q => q.Value.ToString()),
+            Headers = Request.Headers.ToDictionary(h => h.Key, h => h.Value.ToString()),
+            ClientIP = HttpContext.Connection.RemoteIpAddress?.ToString()
+        };
+
+        return Ok(result);
+    }
+    [HttpPost("body")]
+    public IActionResult RepostBody([FromBody] JsonElement body)
+    {
+        var result = new
+        {
+            receivedBody = body
+        };
+
+        return Ok(result);
+    }
+}
